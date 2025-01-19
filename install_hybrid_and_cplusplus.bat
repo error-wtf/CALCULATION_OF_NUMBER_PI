5458069d57ce0036fc6bfaee14072f4431054f3a
LinoCasu <error-wtf@users.noreply.github.com>
2025-01-19 15:40:10 +0100

    HYBRID DEPENCECIES AND START

--- install_hybrid_and_cplusplus.bat
+++ install_hybrid_and_cplusplus.bat
@@ -2,7 +2,5 @@
 echo Installing dependencies for hybrid calculation (Python)...
 pip install mpmath
 echo Dependencies installed!
-
-echo Preparing C++ environment...
-rem Add other installation steps for C++ dependencies if required
-echo C++ environment ready!
+echo starting script
+python .\pi-hybrid.py
